36bf15afb70dabbefec161ac8b3422d8b8bad47a
grmbyrn <graemebyrne_89@hotmail.com>
2022-03-15 12:31:26 +0000

    started video section

--- src/container/Intro/Intro.jsx
+++ src/container/Intro/Intro.jsx
@@ -1,11 +1,22 @@
-import React from 'react';
+import React, {useRef} from 'react';
+import {BsFillPlayFill, BsPauseFill} from 'react-icons/bs'
 
+import {meal} from '../../constants'
 import './Intro.css';
 
-const Intro = () => (
-  <div>
-    Intro
-  </div>
-);
+const Intro = () => {
+  const videRef = useRef()
 
+    return (
+    <div className='app__video'>
+      <video 
+        src={meal} 
+        type='video/mp4' 
+        loop 
+        controls={false} 
+        muted 
+      />
+    </div>
+  )
+}
 export default Intro;
